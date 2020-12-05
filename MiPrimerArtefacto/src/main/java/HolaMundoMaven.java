217639af1ca2e2ab14b6253ee1950ae083bc964b
karco83 <karco_83@hotmail.com>
2020-12-05 10:39:48 +0100

    second commit

--- MiPrimerArtefacto/src/main/java/HolaMundoMaven.java
+++ MiPrimerArtefacto/src/main/java/HolaMundoMaven.java
@@ -1,9 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
 
 public class HolaMundoMaven {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Maven 2");
-
+        System.out.println(StringUtils.capitalize("hola mundo Maven Utils"));
 	}
 
 }
